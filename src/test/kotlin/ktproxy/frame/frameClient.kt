9cb5d7e817faefd673681572c45ec46d173e2a18
Sherlock Holo <sherlockya@gmail.com>
2018-02-24 14:15:54 +0800

    Add Frame test

---
+++ src/test/kotlin/ktproxy/frame/frameClient.kt
@@ -0,0 +1,21 @@
+package ktproxy.frame
+
+import kotlinx.coroutines.experimental.nio.aConnect
+import kotlinx.coroutines.experimental.nio.aWrite
+import kotlinx.coroutines.experimental.runBlocking
+import java.net.InetSocketAddress
+import java.nio.ByteBuffer
+import java.nio.channels.AsynchronousSocketChannel
+
+fun main(args: Array<String>) = runBlocking<Unit> {
+    val socketChannel = AsynchronousSocketChannel.open()
+    socketChannel.aConnect(InetSocketAddress("127.0.0.2", 4567))
+    val buffer = ByteBuffer.allocate(8192)
+    val frame = Frame(FrameType.CLIENT, FrameContentType.BINARY, "sherlock".toByteArray())
+    buffer.put(frame.frameByteArray)
+    buffer.flip()
+    socketChannel.aWrite(buffer)
+    buffer.clear()
+    val data = Frame.buildFrame(socketChannel, buffer, FrameType.SERVER).content
+    println(String(data))
+}
---
+++ src/test/kotlin/ktproxy/frame/frameServer.kt
@@ -0,0 +1,21 @@
+package ktproxy.frame
+
+import kotlinx.coroutines.experimental.nio.aAccept
+import kotlinx.coroutines.experimental.nio.aWrite
+import kotlinx.coroutines.experimental.runBlocking
+import java.io.File
+import java.net.InetSocketAddress
+import java.nio.ByteBuffer
+import java.nio.channels.AsynchronousServerSocketChannel
+
+fun main(args: Array<String>) = runBlocking<Unit> {
+    val serverSocketChannel = AsynchronousServerSocketChannel.open()
+    serverSocketChannel.bind(InetSocketAddress("127.0.0.2", 4567))
+    val socketChannel = serverSocketChannel.aAccept()
+    val buffer = ByteBuffer.allocate(8192)
+    val frame = Frame.buildFrame(socketChannel, buffer, FrameType.CLIENT)
+    println(String(frame.content))
+    buffer.put(Frame(FrameType.SERVER, FrameContentType.BINARY, File("/tmp/randomfile").readBytes()).frameByteArray)
+    buffer.flip()
+    socketChannel.aWrite(buffer)
+}
