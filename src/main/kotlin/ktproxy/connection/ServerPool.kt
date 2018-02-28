2cc022823a29d7fa82ebe3d2cd16cadcf120b546
Sherlock Holo <sherlockya@gmail.com>
2018-02-28 11:24:51 +0800

    Add ServerPool

---
+++ src/main/kotlin/ktproxy/connection/ServerPool.kt
@@ -0,0 +1,35 @@
+package ktproxy.connection
+
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.channels.LinkedListChannel
+import kotlinx.coroutines.experimental.nio.aAccept
+import java.net.InetSocketAddress
+import java.nio.channels.AsynchronousServerSocketChannel
+
+class ServerPool(private val proxyAddr: String, private val proxyPort: Int, private val key: ByteArray) {
+    private val pool = LinkedListChannel<ServerConnection>()
+
+    private suspend fun init() {
+        val serverSocketChannel = AsynchronousServerSocketChannel.open()
+        serverSocketChannel.bind(InetSocketAddress(proxyAddr, proxyPort))
+        async {
+            while (true) {
+                val socketChannel = serverSocketChannel.aAccept()
+                val connection = ServerConnection(socketChannel, key)
+                try {
+                    connection.init()
+                    pool.offer(connection)
+                } finally {
+                }
+            }
+        }
+    }
+
+    suspend fun getConn(): ServerConnection {
+        val connection = pool.receive()
+        connection.shutdownStatus = 0
+        return connection
+    }
+
+    fun putConn(connection: ServerConnection) = pool.offer(connection)
+}
